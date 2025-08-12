7fc0bfe858d3fab30773bc5590d3103002f8029b
Borys Ziukov <ziukoff1985@gmail.com>
2025-08-12 14:14:55 +0300

    Add general logic

--- src/index.js
+++ src/index.js
@@ -1,49 +1,85 @@
-"use strict";
+'use strict';
 
-const number = prompt("Enter a number from 0 to 7 to check permission level");
-
-switch (number) {
-    case "0":
-        console.log(
-            `Number ${number} - your permission level is '---' (No permissions)`
-        );
-        break;
-    case "1":
-        console.log(
-            `Number ${number} - your permission level is '--x' (Execute only)`
-        );
-        break;
-    case "2":
-        console.log(
-            `Number ${number} - your permission level is '-w-' (Write only)`
-        );
-        break;
-    case "3":
-        console.log(
-            `Number ${number} - your permission level is '-wx' (Write and execute)`
-        );
-        break;
-    case "4":
-        console.log(
-            `Number ${number} - your permission level is 'r--' (Read only)`
-        );
-        break;
-    case "5":
-        console.log(
-            `Number ${number} - your permission level is 'r-x' (Read and execute)`
-        );
-        break;
-    case "6":
-        console.log(
-            `Number ${number} - your permission level is 'rw-' (Read and write)`
-        );
-        break;
-    case "7":
-        console.log(
-            `Number ${number} - your permission level is 'rwx' (Read, write, and execute)`
-        );
-        break;
-
-    default:
-        console.log(`Incorrect value entered '${number}' - Access denied`);
+/**
+ * Calculate rating increase if the winner's rating is higher than the loser's rating
+ * @param {number} winnerRate - winner's rating
+ * @param {number} loserRate - loser's rating
+ * @returns {number} increase of rating
+ */
+function calcWinAgainstWeakerOrEqual(winnerRate, loserRate) {
+    const diff = winnerRate - loserRate;
+    if (diff >= 0 && diff <= 2) return 2;
+    if (diff > 2 && diff < 20) return 1;
+    return 0;
 }
+
+/**
+ * Calculate rating increase if the winner's rating is lower than the loser's rating
+ * @param {number} winnerRate - winner's rating
+ * @param {number} loserRate - loser's rating
+ * @returns {number} increase of rating
+ * */
+function calcWinAgainstStronger(winnerRate, loserRate) {
+    return (loserRate - winnerRate + 5) / 3;
+}
+
+/**
+ * Check input correctness
+ * @param {number} winnerRate - winner's rating
+ * @param {number} loserRate - loser's rating
+ * @returns {boolean} true - if input data is correct, false - if not
+ */
+function validateRatings(winnerRate, loserRate) {
+    if (winnerRate < 0 || loserRate < 0) return false;
+    // if (typeof winnerRate !== 'number' || typeof loserRate !== 'number')
+    //     return false;
+    if (!Number.isFinite(winnerRate) || !Number.isFinite(loserRate))
+        return false;
+    return true;
+}
+
+/**
+ * Print result to console
+ * @param {*} winnerRate - winner's rating
+ * @param {*} loserRate - loser's rating
+ * @returns {void} -
+ */
+function printResult(winnerRate, loserRate) {
+    const result = calcNewRate(winnerRate, loserRate);
+    console.log(result);
+    if (Number.isNaN(result)) {
+        console.log(`❌ Некоректні дані: ${winnerRate}, ${loserRate}`);
+        return;
+    }
+    console.log(`✅ Новий рейтинг переможця: ${result} очок`);
+    return;
+}
+
+/**
+ * Calculate new winner's rating
+ * @param {number} winnerRate - winner's rating
+ * @param {number} loserRate - loser's rating
+ * @returns {number} New rating or error message
+ */
+function calcNewRate(winnerRate, loserRate) {
+    const error = validateRatings(winnerRate, loserRate);
+
+    if (!error) {
+        return NaN;
+    }
+
+    if (winnerRate === 0) {
+        return Number(loserRate.toFixed(1));
+    }
+
+    const increase =
+        winnerRate >= loserRate
+            ? calcWinAgainstWeakerOrEqual(winnerRate, loserRate)
+            : calcWinAgainstStronger(winnerRate, loserRate);
+
+    const newWinnerRate = winnerRate + increase;
+    // console.log(newWinnerRate);
+    return Number(newWinnerRate.toFixed(1));
+}
+
+printResult(13, 20);
